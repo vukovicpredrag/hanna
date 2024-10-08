3663ac2d5a9dfdbdc8366d9c9ed6730b291e8374
Predrag Vukovic <predrag.vukovic@infomedia.ba>
2024-10-08 21:02:25 +0200

    fix

--- src/Controller/Admin/SEOCrudController.php
+++ src/Controller/Admin/SEOCrudController.php
@@ -68,6 +68,8 @@ class SEOCrudController extends AbstractCrudController
                 return $action->setLabel('Sačuvaj i vrati se'); // Prilagođena oznaka za "Sačuvaj i vrati se" na stranici za dodavanje
             })
             ->disable(Action::NEW) // This will hide the "Add New" button
+            ->disable(Action::DELETE) // This will hide the "Add New" button
+
             ;
     }
 
