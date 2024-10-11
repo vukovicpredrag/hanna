6643acaa5919838702f7c021e0922380fffe1d89
Predrag Vukovic <predrag.vukovic@infomedia.ba>
2024-10-11 15:42:14 +0200

    test

--- src/Entity/HeroSectionAboutUs.php
+++ src/Entity/HeroSectionAboutUs.php
@@ -8,7 +8,7 @@ use App\Repository\HeroSectionHomeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: HeroSectionHomeRepository::class)]
+#[ORM\Entity(repositoryClass: HeroSectionAboutUSRepository::class)]
 #[ApiResource]
 class HeroSectionAboutUs
 {
